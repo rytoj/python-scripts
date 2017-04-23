d9a11334b2555661362871e2bc4b815f972f393f
rytoj <you@example.com>
2017-04-23 14:39:42 +0300

    Add wind notify compatibility

--- 370.py
+++ 370.py
@@ -7,10 +7,60 @@ import time
 from subprocess import call
 import logging
 import re
+import sys
 
 # Reikalavimai
 # python 3.6
 # pip install BeautifulSoup4
+
+
+if sys.platform[:3] == "win":
+	# Add bubble notifications for wind
+	from win32api import *
+	from win32gui import *
+	import win32con
+	import sys, os
+	import struct
+
+
+	class WindowsBalloonTip:
+		def __init__(self, title, msg):
+			message_map = {
+				win32con.WM_DESTROY: self.OnDestroy,
+			}
+			# Register the Window class.
+			wc = WNDCLASS()
+			hinst = wc.hInstance = GetModuleHandle(None)
+			wc.lpszClassName = "PythonTaskbar"
+			wc.lpfnWndProc = message_map  # could also specify a wndproc.
+			classAtom = RegisterClass(wc)
+			# Create the Window.
+			style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
+			self.hwnd = CreateWindow(classAtom, "Taskbar", style, \
+			                         0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
+			                         0, 0, hinst, None)
+			UpdateWindow(self.hwnd)
+			iconPathName = os.path.abspath(os.path.join(sys.path[0], "balloontip.ico"))
+			icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
+			try:
+				hicon = LoadImage(hinst, iconPathName, \
+				                  win32con.IMAGE_ICON, 0, 0, icon_flags)
+			except:
+				hicon = LoadIcon(0, win32con.IDI_APPLICATION)
+			flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
+			nid = (self.hwnd, 0, flags, win32con.WM_USER + 20, hicon, "tooltip")
+			Shell_NotifyIcon(NIM_ADD, nid)
+			Shell_NotifyIcon(NIM_MODIFY, \
+			                 (self.hwnd, 0, NIF_INFO, win32con.WM_USER + 20, \
+			                  hicon, "Balloon  tooltip", msg, 200, title))
+			# self.show_balloon(title, msg)
+			time.sleep(10)
+			DestroyWindow(self.hwnd)
+
+		def OnDestroy(self, hwnd, msg, wparam, lparam):
+			nid = (self.hwnd, 0)
+			Shell_NotifyIcon(NIM_DELETE, nid)
+			PostQuitMessage(0)  # Terminate the app.
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -145,16 +195,7 @@ def temu_tikrinimas(boards_):
 						reply_id = comments[-1][0]["reply_id"]
 						dubliai = pranesti_apie_dublius(dubliu_tikrinimas(reply_id))
 
-						LOGGER.info(
-							"New content detected\n No: %s %s on /%s/\n name: %s \n description: %s" % (
-								reply_id, dubliai, board_name, name, description))
-						if dubliai:
-							call(
-								["/usr/bin/notify-send", "{}, on /{}/".format(name, board_name),
-								 "! {} !\n{}".format(dubliai, description)])
-						else:
-							call(
-								["/usr/bin/notify-send", "{}, on /{}/".format(name, board_name), description])
+						notify(board_name, description, dubliai, name, reply_id)
 						base_commits = get_comments(boards_)
 						break
 
@@ -162,5 +203,20 @@ def temu_tikrinimas(boards_):
 			LOGGER.info("No changes detected.")
 
 
-# TODO: cross platform
+def notify(board_name, description, dubliai, name, reply_id):
+	if sys.platform[:3] == "lin":
+		LOGGER.info(
+			"New content detected\n No: %s %s on /%s/\n name: %s \n description: %s" % (
+				reply_id, dubliai, board_name, name, description))
+	if dubliai:
+		call(
+			["/usr/bin/notify-send", "{}, on /{}/".format(name, board_name),
+			 "! {} !\n{}".format(dubliai, description)])
+	else:
+		call(
+			["/usr/bin/notify-send", "{}, on /{}/".format(name, board_name), description])
+	if sys.platform[:3] == "win":
+		balloon_tip("{} on /{}/".format(name, board_name), "{}".format(description))
+
+
 temu_tikrinimas(["b", "int", "a", "v"])
