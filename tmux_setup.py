3fbcf177e822b2a54125b512fcf20ba2c1514891
Paulius Gaižauskas <paulius.gaizauskas@visma.com>
2017-09-07 09:18:21 +0300

    Update tmux config

--- tmux_setup.py
+++ tmux_setup.py
@@ -9,6 +9,7 @@ def tmux_move(direction):
     """
     pyautogui.hotkey('ctrl', 'B')
     pyautogui.press(direction)
+    time.sleep(0.5)
 
 
 def enter_command(command_to_execute):
@@ -17,14 +18,14 @@ def enter_command(command_to_execute):
 
 
 def run_as_standalone():
-    time.sleep(5)
     tmux_move("left")
+    
     enter_command("watch --color git status")
     tmux_move("down")
     enter_command("watch --color git remote -v")
-    tmux_move("left")
+    tmux_move("down")
     enter_command("watch --color git diff")
-
+    tmux_move("up")
 
 if __name__ == "__main__":
     run_as_standalone()
